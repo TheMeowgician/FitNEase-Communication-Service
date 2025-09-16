4a187f7193181425aa83bb93e2ad63446e91d232
TheMeowgician <gabsmaglaya@gmail.com>
2025-09-16 12:49:25 +0800

    Added fields to music providers migration file

--- database/migrations/2025_09_16_044649_create_music_providers_table.php
+++ database/migrations/2025_09_16_044649_create_music_providers_table.php
@@ -12,8 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('music_providers', function (Blueprint $table) {
-            $table->id();
+            $table->id('provider_id');
+            $table->string('provider_name')->unique();
+            $table->string('display_name');
+            $table->string('oauth_authorize_url');
+            $table->string('oauth_token_url');
+            $table->string('client_id');
+            $table->text('client_secret');
+            $table->json('supported_scopes');
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
+
+            $table->index(['provider_name', 'is_active']);
         });
     }
 
