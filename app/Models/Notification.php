515476602540729b1c47e27410431c7185a62add
TheMeowgician <gabsmaglaya@gmail.com>
2025-09-16 12:54:07 +0800

    Added fillable attributes, casts, and primary key

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    //
+    protected $primaryKey = 'notification_id';
+
+    protected $fillable = [
+        'user_id',
+        'notification_type',
+        'title',
+        'message',
+        'action_data',
+        'is_read',
+        'is_sent',
+        'email_sent',
+        'scheduled_time',
+        'sent_at',
+        'read_at',
+    ];
+
+    protected $casts = [
+        'action_data' => 'array',
+        'is_read' => 'boolean',
+        'is_sent' => 'boolean',
+        'email_sent' => 'boolean',
+        'scheduled_time' => 'datetime',
+        'sent_at' => 'datetime',
+        'read_at' => 'datetime',
+    ];
 }
