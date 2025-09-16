ec30d5805e7875bb25c3c38a13921c84d2687e33
TheMeowgician <gabsmaglaya@gmail.com>
2025-09-16 12:57:32 +0800

    Added primary key, fillable attributes, casts, replaceVariable in email template model

--- app/Models/EmailTemplate.php
+++ app/Models/EmailTemplate.php
@@ -6,5 +6,40 @@ use Illuminate\Database\Eloquent\Model;
 
 class EmailTemplate extends Model
 {
-    //
+    protected $primaryKey = 'template_id';
+
+    protected $fillable = [
+        'template_name',
+        'template_type',
+        'subject_template',
+        'html_template',
+        'text_template',
+        'variables',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'variables' => 'array',
+        'is_active' => 'boolean',
+    ];
+
+    public function replaceVariables(array $data): array
+    {
+        $subject = $this->subject_template;
+        $html = $this->html_template;
+        $text = $this->text_template;
+
+        foreach ($data as $key => $value) {
+            $placeholder = '{{' . $key . '}}';
+            $subject = str_replace($placeholder, $value, $subject);
+            $html = str_replace($placeholder, $value, $html);
+            $text = str_replace($placeholder, $value, $text);
+        }
+
+        return [
+            'subject' => $subject,
+            'html' => $html,
+            'text' => $text,
+        ];
+    }
 }
