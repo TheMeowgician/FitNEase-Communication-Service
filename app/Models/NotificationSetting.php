87f1f0284e1b8d3543c6fc4308afddbc5bb13980
TheMeowgician <gabsmaglaya@gmail.com>
2025-09-16 12:54:48 +0800

    Added primary key, fillable attributes, and casts for notification model

--- app/Models/NotificationSetting.php
+++ app/Models/NotificationSetting.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class NotificationSetting extends Model
 {
-    //
+    protected $primaryKey = 'setting_id';
+
+    protected $fillable = [
+        'user_id',
+        'notification_type',
+        'enabled',
+        'email_enabled',
+        'push_enabled',
+        'preferences',
+    ];
+
+    protected $casts = [
+        'enabled' => 'boolean',
+        'email_enabled' => 'boolean',
+        'push_enabled' => 'boolean',
+        'preferences' => 'array',
+    ];
 }
